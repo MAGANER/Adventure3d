1bcdf9eaf7990352f2b4ff85eb87587e06ae9bb9
MAGANER <maganer.krik@mail.ru>
2019-07-19 15:08:19 +0300

    rewrote code related to map with my map class

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 #include <math.h>
 #include <unordered_map>
 #include"System\FpsCounter.h"
+#include"System\Map.h"
 #include<iostream>
 using namespace std;
 
 
 
+Map level;
 
 const int screenWidth = 1280;
 const int screenHeight = 720;
@@ -23,97 +25,6 @@ const float cameraHeight = 0.66f; // height of player camera(1.0 is ceiling, 0.0
 const int texture_size = 512; // size(width and height) of texture that will hold all wall textures
 const int texture_wall_size = 128; // size(width and height) of each wall type in the full texture
 
-const float fps_refresh_time = 0.1f; // time between FPS text refresh. FPS is smoothed out over this time
-
-// list of wall texture types, in order as they appear in the full texture
-enum class WallTexture {
-    Smiley,
-    Red,
-    Bush,
-    Sky,
-    Pink,
-    Wallpaper,
-    Dirt,
-    Exit,
-};
-
-// valid wall types and their texture for the world map
-const std::unordered_map<char, WallTexture> wallTypes {
-    {'#', WallTexture::Pink},
-    {'=', WallTexture::Dirt},
-    {'M', WallTexture::Wallpaper},
-    {'N', WallTexture::Bush},
-    {'~', WallTexture::Sky},
-    {'!', WallTexture::Red},
-    {'@', WallTexture::Smiley},
-    {'^', WallTexture::Exit},
-};
-
-// size of the top-down world map in tiles
-const int mapWidth = 24;
-const int mapHeight = 24;
-
-// top-down view of world map
-const char worldMap[] =
-    "~~~~~~~~~~~~~~~~MMM@MMMM"
-    "~..............=M......M"
-    "~..............=M......M"
-    "~..............=@......@"
-    "~..............=M......M"
-    "~....N......N..........M"
-    "~..............=MMM@MM.M"
-    "~..............======M.M"
-    "~..............=MMMMMM.M"
-    "~..............=M......M"
-    "~...N....N.....=M..N..M#"
-    "~.....................M#"
-    "~..............=M..N..M#"
-    "~..............=M.....M#"
-    "~...........N..=MMMMM.M#"
-    "~..............======.=#"
-    "#.!!!!!!.!!!!!!........#"
-    "#.!....!.!..........=..#"
-    "#.!.N..!.!..==..=...=..#"
-    "#...........==..==..=..#"
-    "#.!!!!!!.!..==.........#"
-    "#.######.#..==....=....#"
-    "N......................^"
-    "########################";
-
-// get a tile from worldMap. Not memory safe.
-char getTile(int x, int y) {
-    return worldMap[y * mapWidth + x];
-}
-
-// checks worldMap for errors
-// returns: true on success, false on errors found
-bool mapCheck() {
-    // check size
-    int mapSize = sizeof(worldMap) - 1; // - 1 because sizeof also counts the final NULL character
-    if (mapSize != mapWidth * mapHeight) {
-        fprintf(stderr, "Map size(%d) is not mapWidth * mapHeight(%d)\n", mapSize, mapWidth * mapHeight);
-        return false;
-    }
-
-    for (int y = 0; y < mapHeight; ++y) {
-        for (int x = 0; x < mapWidth; ++x) {
-            char tile = getTile(x, y);
-            // check if tile type is valid
-            if (tile != '.' && wallTypes.find(tile) == wallTypes.end()) {
-                fprintf(stderr, "map tile at [%3d,%3d] has an unknown tile type(%c)\n", x, y, tile);
-                return false;
-            }
-            // check if edges are walls
-            if ((y == 0 || x == 0 || y == mapHeight - 1 || x == mapWidth - 1) &&
-                    tile == '.') {
-                fprintf(stderr, "map edge at [%3d,%3d] is a floor (should be wall)\n", x, y);
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
 // check if a rectangular thing with given size can move to given position without colliding with walls or
 // being outside of the map
 // position is considered the middle of the rectangle
@@ -121,13 +32,13 @@ bool canMove(sf::Vector2f position, sf::Vector2f size) {
     // create the corners of the rectangle
     sf::Vector2i upper_left(position - size / 2.0f);
     sf::Vector2i lower_right(position + size / 2.0f);
-    if (upper_left.x < 0 || upper_left.y < 0 || lower_right.x >= mapWidth || lower_right.y >= mapHeight) {
+	if (upper_left.x < 0 || upper_left.y < 0 || lower_right.x >= level.get_width() || lower_right.y >= level.get_height()) {
         return false; // out of map bounds
     }
     // loop through each map tile within the rectangle. The rectangle could be multiple tiles in size!
     for (int y = upper_left.y; y <= lower_right.y; ++y) {
         for (int x = upper_left.x; x <= lower_right.x; ++x) {
-            if (getTile(x, y) != '.') {
+            if (level.get_tile(x, y) != '.') {
                 return false;
             }
         }
@@ -146,15 +57,11 @@ sf::Vector2f rotateVec(sf::Vector2f vec, float value) {
 
 int main() {
 
-    // if the map is not correct, we can have segmentation faults. So check it.
-    if (!mapCheck()) {
-        fprintf(stderr, "Map is invalid!\n");
-        return EXIT_FAILURE;
-    }
-
-    sf::Font font;
-    if (!font.loadFromFile("data/font/opensans.ttf")) {
-        fprintf(stderr, "Cannot open font!\n");
+     //if the map is not correct, we can have segmentation faults. So check it.
+    if (!level.check_for_errors()) {
+		cout << stderr << "map is invalid!";
+		Time sleep_t = seconds(5.0f);
+		sleep(sleep_t);
         return EXIT_FAILURE;
     }
 
@@ -193,6 +100,7 @@ int main() {
 
 
 	FpsCounter fps_counter;
+
     while (window.isOpen()) {
 		fps_counter.count();
 
@@ -230,7 +138,6 @@ int main() {
 
 
             // handle movement
-
 			float dt = fps_counter.get_delta_time();
             if (moveForward != 0.0f) {
                 sf::Vector2f moveVec = direction * moveSpeed * moveForward * dt;
@@ -353,7 +260,7 @@ int main() {
 
                 color = (color == color1) ? color2 : color1;
 
-                tile = getTile(mapPos.x, mapPos.y);
+                tile = level.get_tile(mapPos.x, mapPos.y);
             }
 
             // calculate lowest and highest pixel to fill in current line
@@ -361,7 +268,7 @@ int main() {
             int drawEnd = groundPixel;
 
             // get position of the wall texture in the full texture
-            int wallTextureNum = (int)wallTypes.find(tile)->second;
+            int wallTextureNum = (int)level.get_wall_types().find(tile)->second;
             sf::Vector2i texture_coords(
                     wallTextureNum * texture_wall_size % texture_size,
                     wallTextureNum * texture_wall_size / texture_size * texture_wall_size
